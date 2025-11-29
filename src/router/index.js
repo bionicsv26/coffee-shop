40c5e0b04edc84f687bd4b29090b45710d44f21a
Sergey Kucherenko <skv026@bk.ru>
2025-11-29 23:00:28 +0300

    added pages: OurCoffeeView, ForYourPleasureView, ContactUsView, ThankYouView. Updated <a> tags on <router-link>

---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+import HeroView from "../views/HeroView";
+import OurCoffeeView from "../views/OurCoffeeView";
+import ForYourPleasureView from "../views/ForYourPleasureView.vue";
+import ContactUsView from "../views/ContactUsView.vue";
+import ThankYouView from "../views/ThankYouView.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+    { path: "/", component: HeroView, },
+    { path: "/our-coffee", component: OurCoffeeView, },
+    { path: "/for-your-pleasure", component: ForYourPleasureView, },
+    { path: "/contact-us", component: ContactUsView, },
+    { path: "/thank-you", component: ThankYouView, }
+]
+
+const router = new VueRouter({
+    mode: "history",
+    routes,
+});
+
+export default router;
