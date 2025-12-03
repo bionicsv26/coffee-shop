2de2fc5466e58b747aba18e2d95fdb5e3c08d95c
Sergey Kucherenko <skv026@bk.ru>
2025-12-03 14:11:43 +0300

    updated the output of links in the NavBar and Footer components

--- src/components/NavBarComponent.vue
+++ src/components/NavBarComponent.vue
@@ -3,28 +3,19 @@
     <ul
       class="header d-flex justify-content-center justify-content-md-start flex-wrap"
     >
-      <li class="header__item">
-        <router-link :to="links[0].link">
-          <img
-            :src="require(`@/assets/logo/${links[0].icon}`)"
-            :alt="links[0].icon"
-          />
-        </router-link>
-      </li>
+      <nav-item classLink="header__item" :link="links.header.link">
+        <img
+          :src="require(`@/assets/logo/${links.header.icon}`)"
+          :alt="links.header.icon"
+        />
+      </nav-item>
+
       <nav-item
+        v-for="link in links.other"
+        :key="link.id"
         classLink="header__item"
-        :link="links[1].link"
-        :text="links[1].text"
-      />
-      <nav-item
-        classLink="header__item"
-        :link="links[2].link"
-        :text="links[2].text"
-      />
-      <nav-item
-        classLink="header__item"
-        :link="links[3].link"
-        :text="links[3].text"
+        :link="link.link"
+        :text="link.text"
       />
     </ul>
   </header>
@@ -36,12 +27,14 @@ export default {
   components: { NavItem },
   data() {
     return {
-      links: [
-        { id: 0, link: "/", icon: "Logo.svg" },
-        { id: 1, text: "Our coffee", link: "/our-coffee" },
-        { id: 2, text: "For your pleasure", link: "/for-your-pleasure" },
-        { id: 3, text: "Contact us", link: "/contact-us" },
-      ],
+      links: {
+        header: { id: 0, link: "/", icon: "Logo.svg" },
+        other: [
+          { id: 1, text: "Our coffee", link: "/our-coffee" },
+          { id: 2, text: "For your pleasure", link: "/for-your-pleasure" },
+          { id: 3, text: "Contact us", link: "/contact-us" },
+        ],
+      },
     };
   },
 };
